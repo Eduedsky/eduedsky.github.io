a7285d7d3aac03beadf227bca0e9f1aa7e2452bf
Karpeds <karpeds@localhost.localdomain>
2020-09-05 03:47:18 +0300

    update

--- projects/weevets/forms/contact.php
+++ projects/weevets/forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'teamcyber540@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -21,19 +21,24 @@
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+  $contact->subject = $_POST['New Client Event Reservation'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
+    'host' => 'gmail.com',
+    'username' => 'teamcyber540@gmail.com',
+    'password' => 'k@k@sungur@1009',
     'port' => '587'
   );
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+  $contact->add_message( $_POST['tel'], 'Telephone No');
+  $contact->add_message( $_POST['date'], 'Event Date');
+  $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['checkbox'], 'Services');
+  $contact->add_message( $_POST['message'], 'Message');
+
 
   echo $contact->send();
 ?>
