3df39a9b094095a0dce7e30f66eb0651e0569987
Eduedsky <edumurigi@gmail.com>
2021-03-01 20:53:33 +0300

    update

--- projects/weevetsschool/forms/contact.php
+++ projects/weevetsschool/forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'teamcyber540@gmail.com';
+  $receiving_email_address = 'contact@example.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -21,20 +21,21 @@
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['New Client Event Reservation'];
+  $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  /*
   $contact->smtp = array(
-    'host' => 'smtp.gmail.com',
-    'username' => 'teamcyber540@gmail.com',
-    'password' => 'k@k@sungur@1009',
+    'host' => 'example.com',
+    'username' => 'example',
+    'password' => 'pass',
     'port' => '587'
   );
+  */
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['phone'], 'Telephone No');
-  $contact->add_message( $_POST['message'], 'Message');
+  $contact->add_message( $_POST['message'], 'Message', 10);
 
   echo $contact->send();
 ?>
